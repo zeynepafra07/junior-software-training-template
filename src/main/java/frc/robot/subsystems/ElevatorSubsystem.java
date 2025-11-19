e6d6457d33e2f10e97b5ee13ec379a0aaffa6aba
Zeynep Afra Yilmaz <zeynepafrayilmaz07@gmail.com>
2025-11-19 23:43:47 +0300

    elevator fixed errors

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -3,112 +3,84 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkBase.ControlType;
-import com.revrobotics.spark.ClosedLoopSlot;
-import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.ClosedLoopConfig;
 import com.revrobotics.spark.config.SparkFlexConfig;
-import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.config.ClosedLoopConfig;
+import com.revrobotics.spark.SparkFlex.MotorType;
+import com.revrobotics.spark.encoder.RelativeEncoder;
 
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.math.controller.PIDController;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.Elevator;
 import frc.robot.Constants.Climb.Levels;
-
+import edu.wpi.first.math.controller.ArmFeedforward;
 
 public class ElevatorSubsystem extends SubsystemBase {
     public SparkFlex masterMotor, slaveMotor;
     public SparkFlexConfig masterConfig, slaveConfig;
+    private final ArmFeedforward feedForward;
+    private final RelativeEncoder encoder;
+
     private final SparkClosedLoopController closedLoopController;
     private final ClosedLoopConfig closedLoopConfig;
-    public PIDController pidController;
-    private final SimpleMotorFeedforward feedforward;
-    private final RelativeEncoder encoder;
 
     public ElevatorSubsystem(){
         masterMotor = new SparkFlex(Constants.Elevator.masterID, MotorType.kBrushless);
         slaveMotor = new SparkFlex(Constants.Elevator.slaveID, MotorType.kBrushless);
         masterConfig = new SparkFlexConfig();
         slaveConfig = new SparkFlexConfig();
+        encoder = masterMotor.getEncoder();
+
         closedLoopController = masterMotor.getClosedLoopController();
         closedLoopConfig = new ClosedLoopConfig();
-        encoder = masterMotor.getEncoder();
-        feedforward = new SimpleMotorFeedforward(Constants.Elevator.kS, Constants.Elevator.kG, Constants.Elevator.kV, Constants.Elevator.kA);
-        pidController = new PIDController(Constants.Elevator.kP, Constants.Elevator.kI, Constants.Elevator.kD);
 
-        encoder.setPositionConversionFactor(1.0);
-        encoder.setVelocityConversionFactor(1.0);
-
+        feedForward = new ArmFeedforward(Constants.Elevator.kS, Constants.Elevator.kG, Constants.Elevator.kV, Constants.Elevator.kA);
+    
         configureMotors();
     }
 
-    public double calculateTargetV(double targetPosition){
-        currentPosition = encoder.getPosition();
-        currentVelocity = encoder.getVelocity();
-        if(targetPosition > currentPosition){
-            targetVelocity = Math.min(Constants.Elevator.cruiseVelocity, (currentVelocity + calculateTargetA(targetPosition)) * 0.13);
-        }
-        else{ targetVelocity = Math.max(-Constants.Elevator.cruiseVelocity, (currentVelocity - calculateTargetA(targetPosition)) * 0.13);
-        }
-        return targetVelocity;
-    }
-    
-    public double calculateTargetA(double targetPosition){
-        currentPosition = encoder.getPosition();
-        if(targetPosition > currentPosition){
-            targetAcceleration = Constants.Elevator.acceleration;
-        }
-        else{
-            targetAcceleration = -Constants.Elevator.acceleration;
-        }
-        return targetAcceleration;
+    public void setPosition(double setPoint){
+        closedLoopController.setReference(setPoint, ControlType.kPosition, ClosedLoopSlot.kSlot0,feedForward.calculate(setPoint, 0) );
+
     }
 
-    public void setPosition(double position){
-        double currentPosition = encoder.getPosition();
-        double pidOutput = pidController.calculate(currentPosition, position);
-        double ffoutpot = feedforward.calculate(calculateTargetV(position), calculateTargetA(position));
-    
-        masterMotor.setVoltage(pidOutput + ffoutpot);
+    public void moveManual(double voltage){
+        masterMotor.setVoltage(voltage);
     }
-
-    public void moveManual(double speed){
-        double position = encoder.getPosition();
-            
-        if(position <= Constants.Climb.Levels.baseHeight && speed < 0){
-            masterMotor.stopMotor();
-        }
-
-        if(position >= Constants.Climb.Levels.maxHeight && speed > 0){
-            masterMotor.stopMotor();
-            return;
-        }
-        
-        masterMotor.setVoltage(speed * 2.0);
-    }
-    
 
     public void stop(){
         masterMotor.stopMotor();
     }
 
     public void reset(){
-        encoder.setPosition(12.0);
+        encoder.setPosition(12);
     }
 
     public void configureMotors(){
-        closedLoopConfig.pid(Constants.Elevator.kP, Constants.Elevator.kI, Constants.Elevator.kD);
+        closedLoopConfig.pid.kP(Constants.Elevator.kP).kI(Constants.Elevator.kI).kD(Constants.Elevator.kD);
+
         masterConfig.idleMode(IdleMode.kBrake).voltageCompensation(12.0).smartCurrentLimit(45).closedLoop.apply(closedLoopConfig);
+        masterConfig.encoder.setPositionConversionFactor(Math.PI * gearratio);
+
         slaveConfig.idleMode(IdleMode.kBrake).follow(masterMotor).voltageCompensation(12.0).smartCurrentLimit(45);
 
         masterMotor.configure(masterConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         slaveMotor.configure(slaveConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
 
+    @Override
+    public void periodic() {
+//
+    }
+
+    public Command reachPosition(double position){
+        return runOnce(() -> setPosition(position)).withName("Elevator Reach Position");
     }
 
 }
