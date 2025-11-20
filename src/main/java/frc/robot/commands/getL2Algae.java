691c8e95a5da158747ca48f6290f75ff41677626
Zeynep Afra Yilmaz <zeynepafrayilmaz07@gmail.com>
2025-11-20 23:46:06 +0300

    added algae commands

---
+++ src/main/java/frc/robot/commands/getL2Algae.java
@@ -0,0 +1,30 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.Constants;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public class getL2Algae extends SequentialCommandGroup{
+    private final ShooterSubsystem shooterSub;
+    private final ElevatorSubsystem elevatorSub;
+
+    publicc getL2Algae(ShooterSubsystem shooterSub, ElevatorSubsystem elevatorSub) {
+        this.shooterSub = shooterSub;
+        this.elevatorSub = elevatorSub;
+
+        addRequirements(shooterSub, elevatorSub);
+
+        addCommands(
+            elevatorSub.setPosition(Constants.Climb.Levels.L2_ALGAE).withTimeout(3),
+            new InstantCommand(() -> shooterSub.algaeIntake(), shooterSub),
+            new WaitCommand(2),
+            new InstantCommand(() -> shooterSub.stop(), shooterSub),
+            elevatorSub.setPosition(Constants.Climb.Levels.HOLD).withTimeout(3)
+        );
+    }
+}
